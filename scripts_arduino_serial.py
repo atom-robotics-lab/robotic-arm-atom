a36853f9a7bc0c11a99ce871297a79952b67ff0f
Chayan24 <93648806+Chayan24@users.noreply.github.com>
2023-10-17 14:02:14 +0530

    Create scripts_arduino_serial.py

---
+++ scripts_arduino_serial.py
@@ -0,0 +1,12 @@
+import serial
+import time
+arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+while True:
+    num = input("Enter a number: ") # Taking input from user
+    value = write_read(num)
+    print(value) # printing the value
