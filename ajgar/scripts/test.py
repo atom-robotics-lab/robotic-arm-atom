c31727ea3160108eea080a07ef8c87c2b9f8e43b
Jayesh Chaudhary <jayeshchaudhary21jan@gmail.com>
2023-10-29 22:27:42 +0530

    Create test.py

---
+++ ajgar/scripts/test.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg 
+from math import pi
+from std_msgs.msg import String
+
+
+class MoveGroupPythonInteface(object):
+    def __init__(self):
+        super(MoveGroupPythonInteface, self).__init__()
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('move_group_python_interface', anonymous=True)
+        robot = moveit_commander.RobotCommander()
+        group_name = "arm"
+        group = moveit_commander.MoveGroupCommander(group_name)
+        
+
+       # planning_frame = group.get_planning_frame()
+       # print ("============ Reference frame: %s" % planning_frame)
+        
+       # eef_link = group.get_end_effector_link()
+       # print ("============ End effector: %s" % eef_link)
+
+        #group_names = robot.get_group_names()
+        #print ("============ Robot Groups:", robot.get_group_names())
+
+        #print ("============ Printing robot state")
+        #print (robot.get_current_state())
+        #print ("")
+
+        #self.box_name = ''
+        #self.robot = robot
+        self.group = group
+        #self.display_trajectory_publisher = display_trajectory_publisher
+        #self.planning_frame = planning_frame
+        #self.eef_link = eef_link
+        #self.group_names = group_names
+
+
+
+    def go_to_joint_state(self):
+
+        group = self.group
+                
+        joint_goal = group.get_current_joint_values()
+        joint_goal[0] = -1
+        joint_goal[1] = 0
+        joint_goal[2] = 0
+        joint_goal[3] = 0
+        # joint_goal[4] = 0
+      
+    
+        group.go(joint_goal, wait=True)
+        group.stop()
+    
+    def go_to_pose_goal(self):
+
+        arm_group = moveit_commander.MoveGroupCommander("arm")
+        gripper_group = moveit_commander.MoveGroupCommander("gripper")
+        arm_group.set_planner_id("RRT")
+        print(arm_group.get_current_pose())
+        print(gripper_group.get_current_pose())
+        
+        
+        arm_group.set_goal_position_tolerance(0.01)  # in meters
+        arm_group.set_goal_orientation_tolerance(0.01)
+        arm_target_pose = geometry_msgs.msg.Pose()
+        arm_target_pose.position.x = 0.4
+        arm_target_pose.position.y = 0.5
+        arm_target_pose.position.z = 0.4
+        arm_target_pose.orientation.x = 0
+        arm_target_pose.orientation.y = 0
+        arm_target_pose.orientation.z = 0
+        arm_target_pose.orientation.w = 1
+        
+        for itr in range (0,10) :
+	
+        	arm_group.set_pose_target(arm_target_pose)
+        	plan = arm_group.plan()
+        	print(itr , "------")
+        	print(plan) 
+      
+    # Plan and execute the arm and gripper motion
+        #arm_group.go()
+        # gripper_group.go()
+
+
+
+def main():
+    interface = MoveGroupPythonInteface()
+    interface.go_to_pose_goal()
+
+
+if __name__ == '__main__':
+    main()
+    
+#rosrun tf static_transform_publisher -6.84879e-07 -0.23105 1.48459 8.47014e-12 -1.37682e-06 5.96021e-06 1  base_link rnd_tf 100
+
+# rosrun tf static_transform_publisher 0.3 0.3 0.3 0 1 0 world rnd_tf 100
