5f2934e513143c10290f3d6dc70840e2cbbf9d55
Bhavay Garg <bhavaygarg2003@gmail.com>
2023-11-23 01:17:07 +0530

    some edits in centoid file

--- ajgar_perception/src/centroid_normal.cpp
+++ ajgar_perception/src/centroid_normal.cpp
@@ -2,27 +2,19 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-
-
-
 #include <iostream>
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
 #include <pcl/registration/icp.h>
 #include <pcl/features/normal_3d.h>
+#include <vector> // Include for std::vector
+#include <sstream> // Include for std::stringstream
 
 typedef pcl::PointXYZ PointType;
 typedef pcl::Normal NormalType;
 typedef pcl::PointCloud<PointType> PointCloud;
 typedef pcl::PointCloud<NormalType> NormalCloud;
 
-void calculateNormals(const PointCloud::Ptr& cloud1, const PointCloud::Ptr& cloud2)
+std::vector<std::string> calculateNormals(const PointCloud::Ptr& cloud1, const PointCloud::Ptr& cloud2)
 {
-    // cloud1 is centroid
-    //PointCloud::Ptr cloud1(new PointCloud);
-    // cloud2 is mask
-    //PointCloud::Ptr cloud2(new PointCloud);
-
     // Create a container for the normals
     NormalCloud::Ptr normals(new NormalCloud);
 
@@ -39,14 +31,17 @@ void calculateNormals(const PointCloud::Ptr& cloud1, const PointCloud::Ptr& clou
     ne.setRadiusSearch(0.03);
     ne.compute(*normals);
 
-    // Print the normals
+    std::vector<std::string> orientations;
+
     // Print the orientations of the normals
     for (size_t i = 0; i < normals->size(); ++i) {
         Eigen::Vector3f normal_vector = normals->points[i].getNormalVector3fMap();
 
         // Convert components to strings and concatenate
         std::stringstream orientation_string;
-        orientation_string << "X: " << normal_vector(0) << ", "<< "Y: " << normal_vector(1) << ", "<< "Z: " << normal_vector(2);
+        orientation_string << "X: " << normal_vector(0) << ", "
+                          << "Y: " << normal_vector(1) << ", "
+                          << "Z: " << normal_vector(2);
 
         // Store the orientation as a string in the vector
         orientations.push_back(orientation_string.str());
