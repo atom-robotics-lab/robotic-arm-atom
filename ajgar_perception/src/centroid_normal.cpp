bf99779f2e663677b9026451958cfc38f2cac890
Bhavay Garg <bhavaygarg2003@gmail.com>
2023-11-23 01:40:06 +0530

    Minor Changes

--- ajgar_perception/src/centroid_normal.cpp
+++ ajgar_perception/src/centroid_normal.cpp
@@ -13,41 +13,43 @@ typedef pcl::Normal NormalType;
 typedef pcl::PointCloud<PointType> PointCloud;
 typedef pcl::PointCloud<NormalType> NormalCloud;
 
-std::vector<std::string> calculateNormals(const PointCloud::Ptr& cloud1, const PointCloud::Ptr& cloud2)
-{
-    // Create a container for the normals
-    NormalCloud::Ptr normals(new NormalCloud);
+extern "C" {
+    std::vector<std::string> calculateNormals(const PointCloud::Ptr& cloud1, const PointCloud::Ptr& cloud2)
+    {
+        // Create a container for the normals
+        NormalCloud::Ptr normals(new NormalCloud);
 
-    // Create a KD-Tree for cloud1
-    pcl::search::KdTree<PointType>::Ptr tree(new pcl::search::KdTree<PointType>);
-    tree->setInputCloud(cloud1);
+        // Create a KD-Tree for cloud1
+        pcl::search::KdTree<PointType>::Ptr tree(new pcl::search::KdTree<PointType>);
+        tree->setInputCloud(cloud1);
 
-    // Initialize normal estimation object
-    pcl::NormalEstimation<PointType, NormalType> ne;
-    ne.setInputCloud(cloud2);
-    ne.setSearchMethod(tree);
+        // Initialize normal estimation object
+        pcl::NormalEstimation<PointType, NormalType> ne;
+        ne.setInputCloud(cloud2);
+        ne.setSearchMethod(tree);
 
-    // Output datasets
-    ne.setRadiusSearch(0.03);
-    ne.compute(*normals);
+        // Output datasets
+        ne.setRadiusSearch(0.03);
+        ne.compute(*normals);
 
-    std::vector<std::string> orientations;
+        std::vector<std::string> orientations;
 
-    // Print the orientations of the normals
-    for (size_t i = 0; i < normals->size(); ++i) {
-        Eigen::Vector3f normal_vector = normals->points[i].getNormalVector3fMap();
+        // Print the orientations of the normals
+        for (size_t i = 0; i < normals->size(); ++i) {
+            Eigen::Vector3f normal_vector = normals->points[i].getNormalVector3fMap();
 
-        // Convert components to strings and concatenate
-        std::stringstream orientation_string;
-        orientation_string << "X: " << normal_vector(0) << ", "
-                          << "Y: " << normal_vector(1) << ", "
-                          << "Z: " << normal_vector(2);
+            // Convert components to strings and concatenate
+            std::stringstream orientation_string;
+            orientation_string << "X: " << normal_vector(0) << ", "
+                            << "Y: " << normal_vector(1) << ", "
+                            << "Z: " << normal_vector(2);
 
-        // Store the orientation as a string in the vector
-        orientations.push_back(orientation_string.str());
+            // Store the orientation as a string in the vector
+            orientations.push_back(orientation_string.str());
+        }
+
+        return orientations;
     }
-
-    return orientations;
 }
 
 int main(int argc, char** argv)
